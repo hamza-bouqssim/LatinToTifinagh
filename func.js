b878b9de2518f88ebefd48e1653f2fa10dd1f0d4
bouqssim1 <hamzaboukssim6@gmail.com>
2022-12-26 04:51:47 +0100

    reading the user input and output the result in a paragraph

--- func.js
+++ func.js
@@ -1,55 +1,64 @@
-function latinToTinifinagh(str) {
-    const charMap = {
-      'a': 'ⴰ',
-      'b': 'ⴱ',
-      'c': 'ⵛ',
-      'd': 'ⴷ',
-      'e': 'ⴻ',
-      'f': 'ⴼ',
-      'g': 'ⴳ',
-      'h': 'ⵀ',
-      'i': 'ⵉ',
-      'j': 'ⵊ',
-      'k': 'ⴽ',
-      'l': 'ⵍ',
-      'm': 'ⵎ',
-      'n': 'ⵏ',
-      'o': 'ⵓ',
-      'q': 'ⵇ',
-      'r': 'ⵔ',
-      's': 'ⵙ',
-      't': 'ⵜ',
-      'u': 'ⵓ',
-      'w': 'ⵡ',
-      'x': 'ⵅ',
-      'y': 'ⵢ',
-      'z': 'ⵥ',
-      'v': 'ⵖ',
-      'Ɣ': 'ⵖ',
-      'D': 'ⴹ',
-      'Ḍ': 'ⴹ',
-      'H': 'ⵃ',
-      'Ḥ': 'ⵃ',
-      'R': 'ⵕ',
-      'Ṛ': 'ⵕ',
-      'Z': 'ⵥ',
-      'Ẓ': 'ⵥ',
-      'S': 'ⵚ',
-      'Ṣ': 'ⵚ',
+const txt = document.getElementById('string');
+const btn = document.getElementById('btn');
+const out1 = document.getElementById('out');
 
-    };
+function latinToTinifinagh() {
+  // Get the value of the input field
+  let _input = txt.value;
+
+  const charMap = {
+    'a': 'ⴰ',
+    'b': 'ⴱ',
+    'c': 'ⵛ',
+    'd': 'ⴷ',
+    'e': 'ⴻ',
+    'f': 'ⴼ',
+    'g': 'ⴳ',
+    'h': 'ⵀ',
+    'i': 'ⵉ',
+    'j': 'ⵊ',
+    'k': 'ⴽ',
+    'l': 'ⵍ',
+    'm': 'ⵎ',
+    'n': 'ⵏ',
+    'o': 'ⵓ',
+    'q': 'ⵇ',
+    'r': 'ⵔ',
+    's': 'ⵙ',
+    't': 'ⵜ',
+    'u': 'ⵓ',
+    'w': 'ⵡ',
+    'x': 'ⵅ',
+    'y': 'ⵢ',
+    'z': 'ⵥ',
+    'v': 'ⵖ',
+    'Ɣ': 'ⵖ',
+    'D': 'ⴹ',
+    'Ḍ': 'ⴹ',
+    'H': 'ⵃ',
+    'Ḥ': 'ⵃ',
+    'R': 'ⵕ',
+    'Ṛ': 'ⵕ',
+    'Z': 'ⵥ',
+    'Ẓ': 'ⵥ',
+    'S': 'ⵚ',
+    'Ṣ': 'ⵚ',
+  };
     
-    let result = '';
+  let result = '';
 
-    for (let i = 0; i < str.length; i++) {
-      let char = str[i];
-      if (charMap[char] !== undefined) {
-        result += charMap[char];
-      } 
-      else {
-        result += char;
-      }
+  for (let i = 0; i < _input.length; i++) {
+    let char = _input[i];
+    if (charMap[char] !== undefined) {
+      result += charMap[char];
+    } 
+    else {
+      result += char;
     }
-    return result;
   }
+  out1.innerHTML = result;
+}
+
+btn.addEventListener('click', latinToTinifinagh);
+
   
